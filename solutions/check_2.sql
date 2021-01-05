890b82a1af74f36015073ce673938eaff29ed3b0
issaZananiri <https://github.com/issaZananiri>
2021-01-06 01:46:04 +0200

    .

--- solutions/check_2.sql
+++ solutions/check_2.sql
@@ -1,1 +1,12 @@
 USE sql_intro;
+SELECT 
+    item_purchased, amount, 
+    cu.name AS cust_name, 
+    co.name AS comp_name
+FROM 
+    transaction AS tr, 
+    customer AS cu, 
+    company AS co
+WHERE 
+    tr.customer_id = cu.id AND
+    tr.company_id = co.id;
--- solutions/check_3.sql
+++ solutions/check_3.sql
@@ -1,1 +1,7 @@
 USE sql_intro;
+INSERT INTO student_teacher VALUES(1, 1);
+INSERT INTO student_teacher VALUES(1, 2);
+INSERT INTO student_teacher VALUES(2, 1);
+INSERT INTO student_teacher VALUES(2, 2);
+INSERT INTO student_teacher VALUES(2, 3);
+INSERT INTO student_teacher VALUES(3, 1);
